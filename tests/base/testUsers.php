3b3f9293aca06ddecabc1d82e750a9f99f9d97ba
Maxime <Maxime@Yirumax-PC>
2014-11-08 18:18:22 +0100

    Complétion des tests.

--- tests/base/testUsers.php
+++ tests/base/testUsers.php
@@ -12,14 +12,14 @@
  */
 
 include '../../base/Users.php';
+include '../header.html';
 
 /* On liste les utilisateurs déjà présent dans la base */
-echo "<p><b>Test 1 : parcours des utilisateurs</b></p>\n";
+echo "\t<p><b>Test 1 : parcours des utilisateurs</b></p>\n";
 listerTout();
 
-
 /* Dans un premier temps, on créer utilisateur pour l'ajouter à la base */
-echo "<p><b>Test 2 : ajout d'un utilisateur</b></p>\n";
+echo "\t<p><b>Test 2 : ajout d'un utilisateur</b></p>\n";
 $u = new Users();
 $u->username = "Maxime";
 $u->password = "azertyuiop";
@@ -29,28 +29,30 @@ $u->insert();
 echo "SUCCESS.<br/>Son ID : " . $u->user_id . "</p>";
 
 /* Ensuite, on recherche l'utilisateur par son ID */
-echo "<p><b>Test 3 : recherche d'un utilisateur par son id (ici " . $u->user_id . ")</b></p>\n";
+echo "\t<p><b>Test 3 : recherche d'un utilisateur par son id (ici " . $u->user_id . ")</b></p>\n";
 $u2 = Users::findById($u->user_id);
 $u2->afficher();
 
 /* Ensuite, on recherche l'utilisateur par son nom */
-echo "<p><b>Test 4 : recherche d'un utilisateur par son nom (ici " . $u->username . ")</b></p>\n";
+echo "\t<p><b>Test 4 : recherche d'un utilisateur par son nom (ici " . $u->username . ")</b></p>\n";
 echo "Test non implemente encore...\n";
 
 /* Ensuite, on recherche l'utilisateur par son e-mail */
-echo "<p><b>Test 5 : recherche d'un utilisateur par son e-mail (ici " . $u->email . ")</b></p>\n";
+echo "\t<p><b>Test 5 : recherche d'un utilisateur par son e-mail (ici " . $u->email . ")</b></p>\n";
 echo "Test non implemente encore...\n";
 
 /* On fait une mise à jour  */
-echo "<p><b>Test 6 : Mise à jour !</b></p>Modification de l'adresse e-mail...<br/>\n";
+echo "\t<p><b>Test 6 : Mise à jour !</b></p>Modification de l'adresse e-mail...<br/>\n";
 $u->email = "nouvelle adresse e-mail !";
 $u->update();
 listerTout();
 
 /* Test de la suppression */
-echo "<p><b>Test 7 : Suppression !</b></p>\n";
+echo "\t<p><b>Test 7 : Suppression !</b></p>\n";
 $u->delete();
 listerTout();
+
+include '../footer.html';
 
 function listerTout() {
     $all = Users::findAll();
